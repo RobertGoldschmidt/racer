4d89cf63592dee32cd013dc5af736b527d071b94
Robert Goldschmidt <robert.goldschmidt@seitwerk.de>
2026-02-16 16:05:13 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/racer/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   server: {
     host: '0.0.0.0',
